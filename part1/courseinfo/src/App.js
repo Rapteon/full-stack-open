b4d8cdfbf9ba7214bb9d68f4c2a8009dcb27a0ec
rapteon <rapteon@tutanota.com>
2021-07-22 10:30:10 +0530

    Completed Exercise 1.5: step 5: Refactored all variables in "App" component into a single object.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -22,31 +22,33 @@ const Total = (props) => {
   console.log(props)
   return (
    <>
-    <p>Number of exercises {props.exerciseCounts[0].exercises + props.exerciseCounts[1].exercises + props.exerciseCounts[2].exercises}</p>
+    <p>Number of exercises {props.value[0].exercises + props.value[1].exercises + props.value[2].exercises}</p>
    </>
   )
 }
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
     return (
         <div>
-            <Header content = {course}/>
-            <Content values = {parts} />
-            <Total exerciseCounts = {parts} />
+            <Header content = {course.name}/>
+            <Content values = {course.parts} />
+            <Total value = {course.parts} />
         </div>
     )
 }
