6e3f9b7b098082b9aeee0c5f44900124533170a4
rapteon <rapteon@tutanota.com>
2021-12-07 10:10:34 +0530

    exercise 1.1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+const Header = (props) => (
+  <h1>{props.name}</h1>
+)
+
+const Content = (props) => (
+  <>
+    <p>
+      {props.parts[0]} {props.counts[0]}
+    </p>
+    <p>
+    {props.parts[1]} {props.counts[1]}
+    </p>
+    <p>
+      {props.parts[2]} {props.counts[2]}
+    </p>
+  </>
+)
+
+const Total = (props) => {
+  /**
+   * sum is assigned the value created by summing all
+   * elements in the counts array.
+   */
+  const sum = props.counts.reduce((previous, current) => (previous + current));
+  return <p>Number of exercises {sum}</p>
+}
+const App = () => {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name={course}/>
+      <Content parts={[part1, part2, part3]} counts={[exercises1, exercises2, exercises3]}/>
+      <Total counts={[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+export default App
