9c3e78a8658b78226c4442479bf7f8e5590973b9
rapteon <rapteon@tutanota.com>
2021-12-12 09:52:23 +0530

    Percentage of positive feedback and average feedback is now visible.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,11 +10,42 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const giveGoodFeedback = () => setGood(good + 1)
-  const giveNeutralFeedback = () => setNeutral(neutral + 1)
-  const giveBadFeedback = () => setBad(bad + 1)
+  const giveGoodFeedback = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+  const giveNeutralFeedback = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+  const giveBadFeedback = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
 
+  /**
+   * Returns the average feedback received by the cafe
+   */
+  const averageFeedback = () => {
+    const [goodWeight, neutralWeight, badWeight] = [1, 0, -1]
+
+    if (all === 0)
+      return 0
+    else
+      return (good * goodWeight + neutral * neutralWeight + bad * badWeight)/all
+  }
+
+  /**
+   * Returns the percentage of positive feedback received.
+   */
+  const positiveFeedback = () => {
+    if (all === 0)
+      return 0
+    else
+      return (good / all * 100)+' %'
+  }
   return (
     <div>
       <h2>give feedback</h2>
@@ -25,6 +56,9 @@ const App = () => {
       <Display text='good' val={good}/>
       <Display text='neutral' val={neutral}/>
       <Display text='bad' val={bad}/>
+      <Display text='all' val={all}/>
+      <Display text='average' val={averageFeedback()}/>
+      <Display text='positive' val={positiveFeedback()}/>
     </div>
   )
 }
