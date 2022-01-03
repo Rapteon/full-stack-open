65349e24c34a5dcc1f6ca438468f845e115f1b8f
rapteon <rapteon@tutanota.com>
2022-01-03 14:54:26 +0530

    Add and display phonebook names.

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNewName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={ (event) => setNewName(event.target.value)}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addNewName}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons ? persons.map(person => <p key={person.name}>{person.name}</p>) : "..."}
+      {/* ... */}
+    </div>
+  )
+}
+
+export default App
