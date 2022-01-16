1f3ec53383f2553cdbd429f173bb5282d211faa9
rapteon <rapteon@tutanota.com>
2022-01-16 11:15:21 +0530

    Added button with each search result to show country details when clicked.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,46 +1,62 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const CountryDisplay = ({countries, ready}) => {
+const CountryHolder = ({country}) => {
+	if (Object.keys(country).length === 0)
+		return <p></p>
+	return (
+		<div>
+			<h1>{country.name.common}</h1>
+			capital {country.capital.map(cap => <span key={cap}>{cap} </span>)}
+			<p>population {country.population}</p>
+			<h2>languages</h2>
+			<ul>
+			{
+				Object.keys(country.languages).map(lang =>
+					<li key={lang}>{country.languages[lang]}</li>)
+			}
+			</ul>
+			<h1>{country.flag}</h1>
+		</div>
+	)
+}
+
+const CountryDisplay = ({countries, ready, setSelected}) => {
 	if (!ready) {
+		console.log('Not ready')
 		return <p></p>
 	}
 	else if (countries.length > 10) {
+		console.log('Too many matches')
 		return <p>Too many matches, specify another filter</p>
 	}
-	else if (countries.length === 1) {
-		return (
-			<div>
-				<h1>{countries[0].name.common}</h1>
-				capital {countries[0].capital.map(cap => <span key={cap}>{cap} </span>)}
-				<p>population {countries[0].population}</p>
-				<h2>languages</h2>
-				<ul>
-				{
-					Object.keys(countries[0].languages).map(lang =>
-						<li key={lang}>{countries[0].languages[lang]}</li>)
-				}
-				</ul>
-				<h1>{countries[0].flag}</h1>
-			</div>
-		)
-	}
-	else {
+	else if (countries.length >= 1) {
+		console.log('countries < 10 && countries > 1')
 		return (
 			<div>
 				{
 					countries.map(country =>
-						<p key={country.name.common}>{country.name.common}</p>
+							<p key={country.name.common}>{country.name.common}
+								<button key={country.name.common} onClick={(event) => {
+									setSelected(country);
+								}}>show</button>
+							</p>
 					)
 				}
 			</div>
 		)
+	}
+	else {
+		console.log('No countries')
+		setSelected({})
+		return <p>No countries found</p>
 	}
 }
 
 const App = () => {
   const [countries, setCountries] = useState([])
 	const [results , setResults] = useState([])
+	const [selectedCountry, setSelectedCountry] = useState({})
 
 	/*
 	 * The setReady() hook is used to avoid showing
@@ -50,8 +66,10 @@ const App = () => {
 	const [ready, setReady] = useState(false)
 
   const searchHook = () => {
+		//'http://restcountries.com/v3.1/all'
+		//'http://localhost:3001/countries'
     axios
-      .get('http://restcountries.com/v3.1/all')
+      .get('http://localhost:3001/countries')
       .then(response => {
         setCountries(response.data)
 				setResults(response.data)
@@ -77,7 +95,8 @@ const App = () => {
   return (
     <div>
       find countries <input onInput={searchCountry}/>
-			<CountryDisplay countries={results} ready={ready}/>	
+			<CountryDisplay countries={results} ready={ready} setSelected={setSelectedCountry}/>	
+			<CountryHolder country={selectedCountry}/>
     </div>
   )
 }
