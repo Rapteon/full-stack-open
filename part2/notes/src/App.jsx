edc9113103c1b1dc963801a5d509670c057d4fb3
Rapteon <rapteon@tutanota.com>
2026-06-15 22:08:55 +0530

    Added CSS to notes app.

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -2,11 +2,16 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
+
+import "./index.css";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     console.log("effect");
@@ -45,7 +50,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id === id ? updatedNote : note)));
       })
       .catch((err) => {
-        alert(`the note ${note.content} was already deleted from the server.`);
+        setErrorMessage(
+          `Note ${note.content} was already removed from the server`,
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -53,6 +63,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -71,6 +82,7 @@ const App = () => {
         <input type="text" value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
