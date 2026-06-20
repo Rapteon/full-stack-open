fff7b429518a28171edd164c28aa600504b868c7
Rapteon <rapteon@tutanota.com>
2026-06-20 11:57:22 +0530

    Part:2.17:Phonebook step 12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
@@ -9,10 +8,11 @@ import Notification from "./components/Notification";
 
 const App = (props) => {
   const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
   const [filterKey, setFilterKey] = useState("");
-  const [notification, setNotification] = useState(null);
+  const [notification, setNotification] = useState({
+    message: null,
+    isError: false,
+  });
 
   const NOTIFICATION_DELAY = 5000;
 
@@ -22,10 +22,10 @@ const App = (props) => {
     });
   }, []);
 
-  const displayNotification = (message) => {
-    setNotification(message);
+  const displayNotification = (message, isError) => {
+    setNotification({ message, isError });
     setTimeout(() => {
-      setNotification(null);
+      setNotification({ message: null, isError: false });
     }, NOTIFICATION_DELAY);
   };
   const handleAddPerson = (person) => {
@@ -41,16 +41,25 @@ const App = (props) => {
 
         personToUpdate = { ...personToUpdate, number: person.number };
 
-        PersonService.updatePerson(personToUpdate).then((updatedPerson) => {
-          displayNotification(`Updated ${person.name}`);
-          setPersons(
-            persons.map((p) => (p.id === updatedPerson.id ? updatedPerson : p)),
-          );
-        });
+        PersonService.updatePerson(personToUpdate)
+          .then((updatedPerson) => {
+            displayNotification(`Updated ${person.name}`, false);
+            setPersons(
+              persons.map((p) =>
+                p.id === updatedPerson.id ? updatedPerson : p,
+              ),
+            );
+          })
+          .catch(() => {
+            displayNotification(
+              `Information of ${person.name} has already been removed from server`,
+              true,
+            );
+          });
       }
     } else {
       PersonService.addPerson(person).then((newPerson) => {
-        displayNotification(`Added ${person.name}`);
+        displayNotification(`Added ${person.name}`, false);
         setPersons(persons.concat(newPerson));
       });
     }
@@ -60,7 +69,7 @@ const App = (props) => {
     const hasConfirmed = window.confirm(`Delete ${person.name}?`);
     if (hasConfirmed) {
       PersonService.deletePerson(person.id).then(() => {
-        displayNotification(`Deleted ${person.name}`);
+        displayNotification(`Deleted ${person.name}`, false);
         setPersons(persons.filter((p) => p.id !== person.id));
       });
     }
@@ -69,7 +78,10 @@ const App = (props) => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <Notification message={notification} />
+      <Notification
+        message={notification.message}
+        isError={notification.isError}
+      />
       <Filter filterKey={filterKey} setFilterKey={setFilterKey}></Filter>
       <h2>add a new</h2>
       <PersonForm onAddPerson={handleAddPerson}></PersonForm>
--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,11 +1,18 @@
-const Notification = ({ message }) => {
-  const style = {
+const Notification = ({ message, isError }) => {
+  const defaultStyle = {
     border: "2px solid green",
     backgroundColor: "lightgrey",
     color: "green",
     padding: 10,
   };
-  return message === null ? null : <p style={style}>{message}</p>;
+  const errorStyle = {
+    ...defaultStyle,
+    borderColor: "red",
+    color: "red",
+  };
+  return message === null ? null : (
+    <p style={isError ? errorStyle : defaultStyle}>{message}</p>
+  );
 };
 
 export default Notification;
